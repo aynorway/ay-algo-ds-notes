2d0b450be2662c146aa336bedb0e5ab9bdd85acb
aynorway <adrianynorway@gmail.com>
2023-10-10 23:45:10 -0400

    2darray ij vs ji efficiency

---
+++ 0109-2DArray/dynamicArray2DArray.java
@@ -0,0 +1,82 @@
+public class dynamicArray2DArray {
+
+    // 执行效率不一样
+    
+    public static void ij(int[][] a, int rows, int columns) {
+        long sum = 0L;
+        for (int i = 0; i< rows; i++) {
+            for (int j = 0; j< columns; j++) {
+                sum += a[i][j];
+            } 
+        }
+        System.out.println(sum);
+    }
+    
+    public static void ji(int[][] a, int rows, int columns) {
+        long sum = 0L; 
+        for (int j = 0; j< columns; j++) { 
+            for (int i = 0; i < rows; i++) {
+                sum += a[i][j];
+            } 
+            System.out.println(sum);
+        }
+    }
+
+    // public static void main(String[] args) {
+    //     int rows = 1000000; 
+    //     int columns = 14;  
+    //     int[][] a = new int[rows][columns];
+        
+    //     StopWatch sw = new StopWatch();
+        
+    //     sw. start( taskName: "ij");  
+    //     ij(a, rows, columns);  
+    //     sw.stop();
+        
+    //     sw. start( taskName: "ji");  
+    //     ji(a, rows, columns);  
+    //     sw.stop();
+        
+    //     System.out.println(sw.prettyPrint());
+    // }
+
+    // 没有stopwatch, 需要spring 
+
+    public static void main(String[] args) {
+        int rows = 1000000; 
+        int columns = 14;  
+        int[][] a = new int[rows][columns];
+
+        long startTime, endTime;
+        
+        startTime = System.nanoTime();
+        ij(a, rows, columns);
+        endTime = System.nanoTime();
+        System.out.println("Time for ij: " + (endTime - startTime) + " nanoseconds");
+        
+        startTime = System.nanoTime();
+        ji(a, rows, columns);
+        endTime = System.nanoTime();
+        System.out.println("Time for ji: " + (endTime - startTime) + " nanoseconds");
+    }
+}
+
+// 二维数组 2 d array 
+// int[][] array = {
+//     {...}
+//     {...}
+//     {...}
+//     {...}
+//     //...
+// }
+
+// markword 8, cp 类指针 4, size 4, [0],[1],[2]......[对齐内存]
+// 定位, 两次索引, array[i][j]  i是外层 j是内层 
+
+// cpu 读内存 纳秒 10 -9 次方 
+// 运算速度 皮秒 10 -12 次方 
+// 所以读的慢
+// 所以加一个缓存 缓存没有, 去内存找 提升cpu效率 
+
+// 内存中的数据存到缓存里的, 存多少 
+
